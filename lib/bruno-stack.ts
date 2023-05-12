bdf6fc1a6fbdf4906227f03e11a578afea8b3ec8
eechih <eechih@gmail.com>
2023-05-12 16:18:03 +0800

    feat: added ApiGateway resource

--- lib/bruno-stack.ts
+++ lib/bruno-stack.ts
@@ -1,16 +1,21 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import Api from "./api";
+
+interface BrunoStackProps extends cdk.StackProps {
+  readonly domain: string;
+}
 
 export class BrunoStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: BrunoStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const { domain } = props;
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BrunoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const api = new Api(this, "Api", {
+      domain: domain,
+      subdomain: "api",
+      routeHandlers: [],
+    });
   }
 }
