de87d5575da68efa2bed4d5ac990d52bde6954f3
Harry Yeh <eechih@gmail.com>
2023-05-16 22:47:44 +0800

    added updateProduct API

--- lib/product.ts
+++ lib/product.ts
@@ -9,6 +9,8 @@ import { join } from 'path'
 const functionDir = join(__dirname, '..', 'function')
 const functionName = 'product'
 
+const vtlDir = join(__dirname, '..', 'api', 'vtl')
+
 type ProductProps = {
   api: appsync.IGraphqlApi
 }
@@ -87,6 +89,15 @@ export default class Product extends Construct {
       requestMappingTemplate: appsync.MappingTemplate.dynamoDbPutItem(
         appsync.PrimaryKey.partition('id').auto(),
         appsync.Values.projecting('input')
+      ),
+      responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultItem(),
+    })
+
+    dynamoDbDataSource.createResolver('UpdateProductResolver', {
+      typeName: 'Mutation',
+      fieldName: 'updateProduct',
+      requestMappingTemplate: appsync.MappingTemplate.fromFile(
+        join(vtlDir, 'updateProduct.vm')
       ),
       responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultItem(),
     })
